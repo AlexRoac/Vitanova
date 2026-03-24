943d5c39da9e67f9ba6866eec497a01d4c8c990d
jaret <jaret.vite@gmail.com>
2026-03-24 17:06:34 -0600

    PAgina Inicio

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
-import Login from "./pages/Login";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Inicio from './pages/Inicio';
+import './App.css';
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+    
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/inicio" element={<Inicio />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
