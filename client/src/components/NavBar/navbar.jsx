96fc6c63e0dc3f9127bcab8dabbe70836111418e
Valeria <valeria.mendez@iest.edu.mx>
2026-03-26 01:33:41 -0600

    Hice la pagina de error 404 y el icono de user en celular

--- client/src/components/NavBar/navbar.jsx
+++ client/src/components/NavBar/navbar.jsx
@@ -1,28 +1,35 @@
 import "./navbar.css";
-import {default as logoV3} from "../../assets/Logo_Vita3.png";
-import {default as logoV4} from "../../assets/Logo_Vita4.png";
+import { default as logoV3 } from "../../assets/Logo_Vita3.png";
+import { default as logoV4 } from "../../assets/Logo_Vita4.png";
+import { FaUserCircle } from "react-icons/fa";
 
 function NavBar() {
-        const handleLogin = () => {
-        window.location.href = "/Login"
-    }
+  const handleLogin = () => {
+    window.location.href = "/Login";
+  };
 
-    const handleRegister = () => {
-        window.location.href = "/Register"
-    }
-    return (
-        <>
-        <div className="container-nav">
-            <div className="logo-container">
-                <img src={logoV3} alt="Logo V3" className="logoV3" />
-                <img src={logoV4} alt="Logo V4" className="logoV4" />
-            </div>
-            {/* <div className="menubtn-container">
-                <button className="menu-btn" onClick={handleLogin}>Log in</button>
-                <button className="menu-btn" onClick={handleRegister}>Sign up</button>
-            </div> */}
-        </div>
-        </>
-    )
+  const handleRegister = () => {
+    window.location.href = "/Register";
+  };
+
+  return (
+    <div className="container-nav">
+      <div className="logo-container">
+        <img src={logoV3} alt="Logo V3" className="logoV3" />
+        <img src={logoV4} alt="Logo V4" className="logoV4" />
+      </div>
+
+      {/* BOTONES (desktop) */}
+      <div className="menubtn-container">
+        <button className="menu-btn" onClick={handleLogin}>Log in</button>
+        <button className="menu-btn" onClick={handleRegister}>Sign up</button>
+      </div>
+
+     <div className="user-icon" onClick={handleLogin}>
+  <FaUserCircle />
+</div>
+    </div>
+  );
 }
+
 export default NavBar;
