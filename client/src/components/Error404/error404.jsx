c8e6aaef72b2b25e52c34a65a16632d01428c187
AlexRoac <alejandro.roac05@gmail.com>
2026-03-27 13:46:33 -0600

    f...

--- client/src/components/Error404/error404.jsx
+++ client/src/components/Error404/error404.jsx
@@ -1,19 +1,40 @@
 import "./error404.css";
-import { Link } from "react-router-dom";
-function Error404() {
-    return (
-        <>
-        <div className="error-container">
-      <div className="error-box">
-        <h1 className="error-title">404</h1>
-        <p className="error-text">No encontramos esta página</p>
+import { useNavigate } from "react-router-dom";
 
-        <Link to="/login" className="error-btn">
-          Volver al inicio
-        </Link>
+function Error404() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    // Obtenemos de que pagina viene el usuario
+    const referrer = document.referrer;
+    
+    // Obtenemos el dominio actual de tu app (ej: localhost:3000 o tuweb.com)
+    const currentDomain = window.location.origin;
+
+    // 3. Solo retrocedemos si el 'referrer' incluye nuestro dominio
+    // Esto evita que el usuario se salga de tu web al picarle al botón.
+    if (referrer && referrer.includes(currentDomain)) {
+      navigate(-1);
+    } else {
+      // Plan B: Si entró directo o viene de Google/Facebook/etc.
+      navigate("/inicio");
+    }
+  };
+
+  return (
+    <>
+      <div className="error-container">
+        <div className="error-box">
+          <h1 className="error-title">404</h1>
+          <p className="error-text">No encontramos esta página</p>
+
+          <button type="button" onClick={handleGoBack} className="error-btn">
+            Volver
+          </button>
+        </div>
       </div>
-    </div>
-        </>
-    )
+    </>
+  );
 }
+
 export default Error404;
