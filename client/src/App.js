719b0e28e65c6f1eea55ee5b3f0c48880169ba52
AlexRoac <alejandro.roac05@gmail.com>
2026-03-27 13:46:33 -0600

    f...

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Navigate to="/inicio" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/inicio" element={<Inicio />} />
