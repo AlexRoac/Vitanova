78ca19ba0418534a1c44d98db73794fdc0484e13
AlexRoac <alejandro.roac05@gmail.com>
2026-05-03 21:02:48 -0600

    arreglamos el responsive y error en horarios al visualizar en calendario

--- client/src/components/Horarios/GestionarHorarios.jsx
+++ client/src/components/Horarios/GestionarHorarios.jsx
@@ -13,8 +13,9 @@ const GestionarHorarios = () => {
     const psicologoId = user ? user.id : null;
 
     const horasDelDia = [
-        "08:00", "09:00", "10:00", "11:00", "12:00",
-        "13:00", "14:00", "15:00", "16:00", "17:00"
+        "07:00", "08:00", "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00", "15:00", "16:00",
+        "17:00", "18:00", "19:00", "20:00"
     ];
 
     const fechaLabel = fecha.toLocaleDateString('es-MX', {
